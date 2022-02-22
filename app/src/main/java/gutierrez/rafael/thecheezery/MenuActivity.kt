4c27b1048b2c095ddb81c788c743ec56fde0b694
arckenos <73735400+arckenos@users.noreply.github.com>
2022-02-22 11:19:07 -0700

    Pratica 5

--- app/src/main/java/gutierrez/rafael/thecheezery/MenuActivity.kt
+++ app/src/main/java/gutierrez/rafael/thecheezery/MenuActivity.kt
@@ -1,11 +1,20 @@
 package gutierrez.rafael.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks)
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/gutierrez/rafael/thecheezery/Product.kt
@@ -0,0 +1,8 @@
+package gutierrez.rafael.thecheezery
+
+data class Product (
+    var name: String,
+    var image: Int,
+    var description : String,
+    var price : Double
+)

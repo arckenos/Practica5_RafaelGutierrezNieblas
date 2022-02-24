50dd0fa736c354a20e47c079bc7381365878bbc6
arckenos <73735400+arckenos@users.noreply.github.com>
2022-02-23 23:44:16 -0700

    MENUS

--- app/src/main/java/gutierrez/rafael/thecheezery/MenuActivity.kt
+++ app/src/main/java/gutierrez/rafael/thecheezery/MenuActivity.kt
@@ -11,10 +11,39 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnCold: Button = findViewById(R.id.button_cold_drinks)
+        var btnHot: Button = findViewById(R.id.button_hot_drinks)
+        var btnSweets: Button = findViewById(R.id.button_sweets)
+        var btnSalties: Button = findViewById(R.id.button_salties)
 
-        btnCold.setOnClickListener{
-            var intent: Intent = Intent(this, ProductosActivity::class.java)
+        btnCold.setOnClickListener {
+
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "1")
             startActivity(intent)
         }
+
+        btnHot.setOnClickListener {
+
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "2")
+            startActivity(intent)
+
+
+        }
+
+        btnSweets.setOnClickListener {
+
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "3")
+            startActivity(intent)
+
+
+        }
+
+        btnSalties.setOnClickListener {
+
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "4")
+            startActivity(intent)
+
+
+        }
+
     }
 }
